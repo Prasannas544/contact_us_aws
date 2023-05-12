6a27e920a83215054203aa23fbccf76986915f98
prasannas544 <prasanna.swain@stepapp.ai>
2023-05-12 21:52:39 +0530

    id recieved

--- server.js
+++ server.js
@@ -36,6 +36,19 @@ app.post("/submit-form-data", (req, res) => {
   };
   res.json(data);
 });
+app.post("/send-device-code", (req, res) => {
+  // Extract form data from the request body
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  const {device_id,user_name} = req.body;
+  quizAppDB(user_name, device_id).catch(console.dir);
+  let data = {
+    status: "200",
+    msg: "Device notif_id sent successfully",
+  };
+  res.json(data);
+});
 
 // Start server listening on specified port
 const PORT = 8080;
@@ -59,6 +72,29 @@ async function run(name, email, topic, message) {
       email: email,
       topic: topic,
       msg: message,
+    });
+    console.log(
+      `${result.insertedCount} documents were inserted with _id: ${result.insertedId}`
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+    console.log("Connection closed");
+  }
+}
+async function quizAppDB(user_name,device_id) {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    const database = client.db("quizApp");
+    const collection = database.collection("notificationsIDs");
+
+    // Insert a document
+    const result = await collection.insertOne({
+      user_name: user_name,
+      device_id:device_id,
     });
     console.log(
       `${result.insertedCount} documents were inserted with _id: ${result.insertedId}`
